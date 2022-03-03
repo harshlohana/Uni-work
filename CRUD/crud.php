6630eb4d5b70f6589b1553831528064add5a3834
Harsh Lalit Lohana <47289608+harshlohana@users.noreply.github.com>
2022-03-03 18:55:59 +0530

    Update crud.php

--- CRUD/crud.php
+++ CRUD/crud.php
@@ -1,29 +1,46 @@
+<?php
+include '../connect.php';
+
+if (isset($_POST['delete'])) {  
+    $deleteid = $_POST['id'];
+    //echo "<script>alert('$deleteid')</script>";  
+	$sqldelete = "DELETE FROM f_customertable WHERE customer_id='$deleteid';";
+	$resultfordelete = mysqli_query($conn, $sqldelete);
+	if ($resultfordelete > 0) {
+		echo "<script>alert('Record Deleted')</script>";
+}else{
+    echo "<script>alert('ERROR')</script>";
+}
+}
+
+if (isset($_POST['add'])) {  
+    $changeid = $_POST['id'];
+    $fname=$_POST['fname'];
+    $lname=$_POST['lname'];
+    $age=$_POST['age'];
+    $eaddress=$_POST['eaddress'];
+    $pnumber=$_POST['pnumber'];
+    //echo "<script>alert('$changeid')</script>";  
+	$sqladd = "UPDATE f_customertable SET first_name='$fname',last_name='$lname',age='$age',email_address='$eaddress',phone_number='$pnumber' WHERE customer_id='$changeid'";
+	$resultforupdate = mysqli_query($conn, $sqladd);
+	if ($resultforupdate > 0) {
+		echo "<script>alert('Record Updated')</script>";
+}else{
+    echo "<script>alert('ERROR')</script>";
+}
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <?php
-
-    include '../connect.php';
-
-    $sql = "SELECT * FROM f_customertable";
-    $exec=mysqli_query($conn, $sql);
-    $row= [];
-    if(mysqli_num_rows($exec)>0){
-        $row= mysqli_fetch_all($exec, MYSQLI_ASSOC);
-       echo json_encode($row);
-    }else{
-        echo "database is empty";
-    }
-
-    $conn->close();
-
-    ?>
+<head>  
     <meta charset="UTF-8">
     <title>My Records</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
     <link href="styles.CSS.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> 
 </head>
 <body>
 <!-- Logo -->
@@ -50,290 +67,92 @@
 <hr>
 <!-- Page Content -->
 <style>
-    table
-    {
-        width: 100%;
-    }
-    table, th, td
-    {
-        border: solid 1px #DDD;
-        border-collapse: collapse;
-        padding: 2px 3px;
-        text-align: center;
-    }
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
-    input[type='button']
-    {
-        cursor: pointer;
-        border: none;
-        color: #FFF;
-    }
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
 
-    input[type='text'], select
-    {
-        text-align: center;
-        border: solid 1px #CCC;
-        width: auto;
-        padding: 2px 3px;
-    }
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
 </style>
+<script>
+
+</script>
 <body>
 <!--Show the app here.-->
-<div id="container" style="width:700px;"></div>
-</body>
-<script>
-    var crudApp = new function () {
-
-        // AN ARRAY OF OBJECTS WITH VALUES.
-        /* this.myRecords = [
-            {ID: '1', First_Name: 'Kyle', Last_Name: 'Humphries', Age: '22', Email_Address: 'kyle@gmail.com', Phone_Number: '7946861458',  Car_ID: '#2'},
-            {ID: '2', First_Name: 'Kyle', Last_Name: 'Humphries', Age: '22', Email_Address: 'kyle@gmail.com', Phone_Number: '7946861458',  Car_ID: '#2'},
-            {ID: '3', First_Name: 'Kyle', Last_Name: 'Humphries', Age: '22', Email_Address: 'kyle@gmail.com', Phone_Number: '7946861458',  Car_ID: '#2'}
-        ] */
-        <?php echo $row ?>
-        this.myRecords = <?php echo json_encode($row); ?>
-        this.category = ['#1', '#2', '#3', '#4'];
-        this.col = [];
-
-        this.createTable = function () {
-
-            // EXTRACT VALUE FOR TABLE HEADER.
-            for (var i = 0; i < this.myRecords.length; i++) {
-                for (var key in this.myRecords[i]) {
-                    if (this.col.indexOf(key) === -1) {
-                        this.col.push(key);
-                    }
-                }
-            }
-
-            // CREATE A TABLE.
-            var table = document.createElement('table');
-            table.setAttribute('id', 'recordsTable');     // SET TABLE ID.
-
-            var tr = table.insertRow(-1);               // CREATE A ROW (FOR HEADER).
-
-            for (var h = 0; h < this.col.length; h++) {
-                // ADD TABLE HEADER.
-                var th = document.createElement('th');
-                th.innerHTML = this.col[h].replace('_', ' ');
-                tr.appendChild(th);
-            }
-            // ADD ROWS USING DATA.
-            for (var i = 0; i < this.myRecords.length; i++) {
-
-                tr = table.insertRow(-1);           // CREATE A NEW ROW.
-
-                for (var j = 0; j < this.col.length; j++) {
-                    var tabCell = tr.insertCell(-1);
-                    tabCell.innerHTML = this.myRecords[i][this.col[j]];
-                }
-
-                // DYNAMICALLY CREATE AND ADD ELEMENTS TO TABLE CELLS WITH EVENTS.
-
-                this.td = document.createElement('td');
-
-                // *** CANCEL OPTION.
-                tr.appendChild(this.td);
-                var lblCancel = document.createElement('label');
-                lblCancel.innerHTML = '✖';
-                lblCancel.setAttribute('onclick', 'crudApp.Cancel(this)');
-                lblCancel.setAttribute('style', 'display:none;');
-                lblCancel.setAttribute('title', 'Cancel');
-                lblCancel.setAttribute('id', 'lbl' + i);
-                this.td.appendChild(lblCancel);
-
-                // *** SAVE.
-                tr.appendChild(this.td);
-                var btSave = document.createElement('input');
-
-                btSave.setAttribute('type', 'button');      // SET ATTRIBUTES.
-                btSave.setAttribute('value', 'Save');
-                btSave.setAttribute('id', 'Save' + i);
-                btSave.setAttribute('style', 'display:none;');
-                btSave.setAttribute('onclick', 'crudApp.Save(this)');       // ADD THE BUTTON's 'onclick' EVENT.
-                this.td.appendChild(btSave);
-
-                // *** UPDATE.
-                tr.appendChild(this.td);
-                var btUpdate = document.createElement('input');
-
-                btUpdate.setAttribute('type', 'button');    // SET ATTRIBUTES.
-                btUpdate.setAttribute('value', 'Update');
-                btUpdate.setAttribute('id', 'Edit' + i);
-                btUpdate.setAttribute('style', 'background-color:#44CCEB;');
-                btUpdate.setAttribute('onclick', 'crudApp.Update(this)');   // ADD THE BUTTON's 'onclick' EVENT.
-                this.td.appendChild(btUpdate);
-
-                // *** DELETE.
-                this.td = document.createElement('th');
-                tr.appendChild(this.td);
-                var btDelete = document.createElement('input');
-                btDelete.setAttribute('type', 'button');    // SET INPUT ATTRIBUTE.
-                btDelete.setAttribute('value', 'Delete');
-                btDelete.setAttribute('style', 'background-color:#ED5650;');
-                btDelete.setAttribute('onclick', 'crudApp.Delete(this)');   // ADD THE BUTTON's 'onclick' EVENT.
-                this.td.appendChild(btDelete);
-            }
-            // ADD A ROW AT THE END WITH BLANK TEXTBOXES AND A DROPDOWN LIST (FOR NEW ENTRY).
-
-            tr = table.insertRow(-1);           // CREATE THE LAST ROW.
-
-            for (var j = 0; j < this.col.length; j++) {
-                var newCell = tr.insertCell(-1);
-                if (j >= 1) {
-                    if (j == 6) {   // DROPDOWN LIST AT THE SECOND COLUMN
-
-                        var select = document.createElement('select');      // CREATE AND ADD A DROPDOWN LIST.
-                        select.innerHTML = '<option value=""></option>';
-                        for (k = 0; k < this.category.length; k++) {
-                            select.innerHTML = select.innerHTML +
-                                '<option value="' + this.category[k] + '">' + this.category[k] + '</option>';
-                        }
-                        newCell.appendChild(select);
-                    } else {
-                        var tBox = document.createElement('input');          // CREATE AND ADD A TEXTBOX.
-                        tBox.setAttribute('type', 'text');
-                        tBox.setAttribute('value', '');
-                        newCell.appendChild(tBox);
-                    }
-                }
-            }
-            this.td = document.createElement('td');
-            tr.appendChild(this.td);
-
-            var btNew = document.createElement('input');
-
-            btNew.setAttribute('type', 'button');       // SET ATTRIBUTES
-            btNew.setAttribute('value', 'Create');
-            btNew.setAttribute('id', 'New' + i);
-            btNew.setAttribute('style', 'background-color:#207DD1;');
-            btNew.setAttribute('onclick', 'crudApp.CreateNew(this)');       // ADD THE BUTTON's 'onclick' EVENT
-            this.td.appendChild(btNew);
-
-            var div = document.getElementById('container');
-            div.innerHTML = '';
-            div.appendChild(table);    // ADD THE TABLE TO THE WEB PAGE.
-        };
-        // ****** OPERATIONS START.
-
-        // CANCEL.
-        this.Cancel = function (oButton) {
-
-            // HIDE THIS BUTTON.
-            oButton.setAttribute('style', 'display:none; float:none;');
-
-            var activeRow = oButton.parentNode.parentNode.rowIndex;
-
-            // HIDE THE SAVE BUTTON.
-            var btSave = document.getElementById('Save' + (activeRow - 1));
-            btSave.setAttribute('style', 'display:none;');
-
-            // SHOW THE UPDATE BUTTON AGAIN.
-            var btUpdate = document.getElementById('Edit' + (activeRow - 1));
-            btUpdate.setAttribute('style', 'display:block; margin:0 auto; background-color:#44CCEB;');
-
-            var tab = document.getElementById('recordsTable').rows[activeRow];
-
-            for (i = 0; i < this.col.length; i++) {
-                var td = tab.getElementsByTagName("td")[i];
-                td.innerHTML = this.myRecords[(activeRow - 1)][this.col[i]];
-            }
-        }
-
-        // EDIT DATA
-        this.Update = function (oButton) {
-            var activeRow = oButton.parentNode.parentNode.rowIndex;
-            var tab = document.getElementById('recordsTable').rows[activeRow];
-
-            // SHOW A DROPDOWN LIST WITH A LIST OF CATEGORIES.
-            for (i = 1; i < 7; i++) {
-                if (i == 6) {
-                    var td = tab.getElementsByTagName("td")[i];
-                    var ele = document.createElement('select');      // DROPDOWN LIST.
-                    ele.innerHTML = '<option value="' + td.innerText + '">' + td.innerText + '</option>';
-                    for (k = 0; k < this.category.length; k++) {
-                        ele.innerHTML = ele.innerHTML +
-                            '<option value="' + this.category[k] + '">' + this.category[k] + '</option>';
-                    }
-                    td.innerText = '';
-                    td.appendChild(ele);
-                } else {
-                    var td = tab.getElementsByTagName("td")[i];
-                    var ele = document.createElement('input');      // TEXTBOX.
-                    ele.setAttribute('type', 'text');
-                    ele.setAttribute('value', td.innerText);
-                    td.innerText = '';
-                    td.appendChild(ele);
-                }
-            }
-
-            var lblCancel = document.getElementById('lbl' + (activeRow - 1));
-            lblCancel.setAttribute('style', 'cursor:pointer; display:block; width:20px; float:left; position: absolute;');
-
-            var btSave = document.getElementById('Save' + (activeRow - 1));
-            btSave.setAttribute('style', 'display:block; margin-left:30px; float:left; background-color:#2DBF64;');
-
-            // HIDE THIS BUTTON.
-            oButton.setAttribute('style', 'display:none;');
-        };
-
-        // DELETE DATA.
-        this.Delete = function (oButton) {
-            var activeRow = oButton.parentNode.parentNode.rowIndex;
-            this.myRecords.splice((activeRow - 1), 1);    // DELETE THE ACTIVE ROW
-            this.createTable();                         // REFRESH THE TABLE
-        };
-
-        // SAVE DATA.
-        this.Save = function (oButton) {
-            var activeRow = oButton.parentNode.parentNode.rowIndex;
-            var tab = document.getElementById('recordsTable').rows[activeRow];
-
-            // UPDATE myRecords ARRAY WITH VALUES.
-            for (i = 1; i < this.col.length; i++) {
-                var td = tab.getElementsByTagName("td")[i];
-                if (td.childNodes[0].getAttribute('type') == 'text' || td.childNodes[0].tagName == 'SELECT') {  // CHECK IF ELEMENT IS A TEXTBOX OR SELECT.
-                    this.myRecords[(activeRow - 1)][this.col[i]] = td.childNodes[0].value;      // SAVE THE VALUE.
-                }
-            }
-            this.createTable();     // REFRESH THE TABLE.
-        }
-
-        // CREATE NEW.
-        this.CreateNew = function (oButton) {
-            var activeRow = oButton.parentNode.parentNode.rowIndex;
-            var tab = document.getElementById('recordsTable').rows[activeRow];
-            var obj = {};
-
-            // ADD NEW VALUE TO ARRAY.
-            for (i = 1; i < this.col.length; i++) {
-                var td = tab.getElementsByTagName("td")[i];
-                if (td.childNodes[0].getAttribute('type') == 'text' || td.childNodes[0].tagName == 'SELECT') {
-                    // CHECK IF ELEMENT IS A TEXTBOX OR SELECT.
-                    var txtVal = td.childNodes[0].value;
-                    if (txtVal != '') {
-                        obj[this.col[i]] = txtVal.trim();
-                    } else {
-                        obj = '';
-                        alert('all fields are compulsory');
-                        break;
-                    }
-                }
-            }
-            obj[this.col[0]] = this.myRecords.length + 1;     // NEW ID.
-
-            if (Object.keys(obj).length > 0) {      // CHECK IF OBJECT IS NOT EMPTY.
-                this.myRecords.push(obj);             // PUSH (ADD) DATA TO THE ARRAY.
-                this.createTable();                 // REFRESH THE TABLE.
-            }
-        }
-    }
-    crudApp.createTable();
-</script>
-
-    <!-- Footer-->
-        <hr>
-            <div>
-                <p style="color: black"> <b>FOLLOW MOTOR HUB</b></p>
-            </div>
+<div id="container" style="max-width:1300px">
+  <table>
+  <thead>
+        <tr>
+          <th>CustomerId</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Age</th>
+          <th>Phone number</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+ <tbody>
+ <?php
+        $sql = "SELECT * FROM f_customertable";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_num_rows($result);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) { ?>
+               <tr id="<?php echo "div".$row['customer_id'];?>">
+                <td><?php echo $row['customer_id']; ?></td>
+                <td><?php echo $row['first_name']; ?></td>
+                <td><?php echo $row['last_name']; ?></td>
+                <td><?php echo $row['email_address']; ?></td>
+                <td><?php echo $row['age']; ?></td>
+                <td><?php echo $row['phone_number']; ?></td>
+                <td>
+                    <form action="" method="post">
+                        <input type="hidden" name="id" value="<?php echo $row['customer_id'];?>">
+                        <button type="submit" id="<?php echo $row['customer_id'];?>" onclick="editFunction(this.id)">Edit</button>
+                        <button type="submit" name="delete">Delete</button>
+                    </form>
+                </td>
+            </tr>  
+        
+        <?php }}
+        $conn->close();
+        ?>
+ </tbody>
+</table>
+  </div>
+</div>
+    <div>
+        <p style="color: black"> <b>FOLLOW MOTOR HUB</b></p>
+    </div>
+ <script>
+   function editFunction(id){
+    var recordDiv = document.getElementById("div"+id);
+    recordDiv.innerHTML=`
+    <form id="form${id}" action="" method="POST">
+    <td><input form="form${id}" type="text" value="${id}" disabled=disabled></input></td>
+    <td><input form="form${id}" type="text" name="fname"></input></td>
+    <td><input form="form${id}" type="text" name="lname"></input></td>
+    <td><input form="form${id}" type="text" name="eaddress"></input></td>
+    <td><input form="form${id}" type="text" name="age"></input></td>
+    <td><input form="form${id}" type="text" name="pnumber"></input></td>
+    <td>
+    <input form="form${id}" type="hidden" name="id" value="${id}">
+    <button form="form${id}" type="submit" name="add" >Submit</button>
+    <button form="form${id}" type="submit">Cancel</button>
+    </td>
+    </form>
+    `;
+}    
+ </script>   
+</body>    
 </html>
